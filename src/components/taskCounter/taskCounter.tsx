d48f8142c2f8e432ad772009b18ffc0211af3fb6
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2023-02-23 22:31:01 -0500

    CHORE: Emitting Correct Label

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -1,8 +1,11 @@
 import React, { FC, ReactElement } from 'react';
 import { Avatar, Box, Typography } from '@mui/material';
+
 import { ITaskCounter } from './interfaces/ITaskCounter';
 import { Status } from '../createTaskForm/enums/Status';
 import { emitCorrectBorderColor } from './helpers/emitCorrectBorderColor';
+import { emitCorrectLabel } from './helpers/emitCorrectLabel';
+import PropTypes from 'prop-types';
 
 export const TaskCounter: FC<ITaskCounter> = ( props ): ReactElement => {
   // Destructure props
@@ -29,7 +32,7 @@ export const TaskCounter: FC<ITaskCounter> = ( props ): ReactElement => {
           }}
         >
           <Typography color="#ffffff" variant="h4" >
-            10
+            {count}
           </Typography>
         </Avatar>
         <Typography
@@ -38,9 +41,18 @@ export const TaskCounter: FC<ITaskCounter> = ( props ): ReactElement => {
           fontSize="20px"
           variant="h5"
         >
-          Subtitle
+          {emitCorrectLabel(status)}
         </Typography>
       </Box>
     </>  
-  )
+  );
+};
+
+TaskCounter.propTypes = {
+  count: PropTypes.number,
+  status: PropTypes.oneOf([ 
+    Status.todo, 
+    Status.inProgress, 
+    Status.completed 
+  ]),
 };
