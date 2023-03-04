6c8f43fc68de01c1e6e73713b617754f66ed4f72
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2023-03-04 15:59:14 -0500

    CHORE: TaskStatusChangedContext

---
+++ src/context/TaskStatusChangedContext/TaskStatusChangedContext.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, FC, ReactElement, PropsWithChildren,useState } from "react";
+
+export const TaskStatusChangedContext = createContext({
+  updated: false,
+  toogle: () => {},
+
+});
+
+export const TaskStatusChangedContextProvider: FC<
+  PropsWithChildren
+> = (props):ReactElement => {
+  const [updated, setUpdated] = useState(false);
+  
+  function toogleHandler () {
+    updated ? setUpdated(false) : setUpdated(true);
+  }
+
+  return (
+    <TaskStatusChangedContext.Provider 
+      value={{
+        updated: updated,
+        toogle: toogleHandler,
+      }} 
+    >
+      {props.children}
+    </TaskStatusChangedContext.Provider>
+  )
+};
